36e5b616bb51012a93f19b577675b3593cb9d70a
Frank A. Krueger <fak@praeclarum.org>
2017-10-07 14:18:15 -0700

    Scaffold the home screen of the app

--- Centerstone/Centerstone/Models/HIF.cs
+++ Centerstone/Centerstone/Models/HIF.cs
@@ -1,27 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace Centerstone.Models
 {
-	public class HIF
+	public class HIF : BaseModel
 	{
 		public string Zip { get; set; }
 
-		public System.Collections.ObjectModel.ObservableCollection<Person> People { get; set; }
+		public ObservableCollection<Person> People { get; set; } =
+			new ObservableCollection<Person> ();
+		
 		public IEnumerable<Person> Adults => People.Where (x => x.DesignatedAdult);
 		public IEnumerable<Person> Children => People.Where (x => !x.DesignatedAdult);
 
-		string ContactEmail;
-		string ContactPhone;
+		public string ContactEmail { get; set; } = "";
+		public string ContactPhone { get; set; } = "";
 
 		public decimal MonthlyHouseholdIncome { get; set; }
 
-		HouseholdType HouseholdType;
-		string HouseholdStatus;
+		public HouseholdType HouseholdType { get; set; } = HouseholdType.Family1To3;
+		public HouseholdStatus HouseholdStatus { get; set; } = HouseholdStatus.OwnOrBuy;
+
+		public bool TipsAgreedTo { get; set; }
 
 		public HIF ()
 		{
+			People.CollectionChanged += People_CollectionChanged;
+		}
+
+		void People_CollectionChanged (object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+		{
+			OnPropertyChanged ("People");
 		}
 	}
 }
