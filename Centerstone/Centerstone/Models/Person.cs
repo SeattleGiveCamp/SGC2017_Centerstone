190e4b2868dbaeb3848d486ffee75c51204bfa16
yichiang <yi_chiang@outlook.com>
2017-10-07 15:56:50 -0700

    Update View Model and Person.cs

--- Centerstone/Centerstone/Models/Person.cs
+++ Centerstone/Centerstone/Models/Person.cs
@@ -6,8 +6,8 @@ namespace Centerstone.Models
 	public class Person
 	{
 		public bool DesignatedAdult { get; set; }
-
-		public DateTime DateOfBirth { get; set; }
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
 
 		public Image SocialSecurityImage { get; set; }
 
