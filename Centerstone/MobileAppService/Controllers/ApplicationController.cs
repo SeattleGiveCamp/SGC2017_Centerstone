4ecfcfe665da5c45b1d995936cc4de7afe6545aa
David Houchin <davidghouchin@gmail.com>
2017-10-07 13:24:25 -0700

    Add stubs for Application controller and repository

---
+++ Centerstone/MobileAppService/Controllers/ApplicationController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Centerstone.MobileAppService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Centerstone.MobileAppService.Controllers
+{
+    [Route("api/[controller]")]
+    public class ApplicationController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Application> Get()
+        {
+            return new Application();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return new Application();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]Application app)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Application app)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Centerstone/MobileAppService/Data/IApplicationRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Centerstone.MobileAppService.Models;
+
+namespace Centerstone.Data
+{
+	public interface IApplicationRepository
+	{
+		void Add(Application app);
+		void Update(Application app);
+		Application Remove(int id);
+		Application Get(int id);
+		IEnumerable<Application> GetAll();
+	}
+}
