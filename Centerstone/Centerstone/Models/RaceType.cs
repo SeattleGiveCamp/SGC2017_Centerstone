050cf9ceb5759ff6fd500e33ea09deaaf1a6a7b0
yichiang <yi_chiang@outlook.com>
2017-10-07 13:09:47 -0700

    Add Models

---
+++ Centerstone/Centerstone/Models/RaceType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Centerstone.Models
+{
+	public enum RaceType
+    {
+        AmericanIndianOrAlaskanNative,
+		Asian,
+        BlackOrAfricanAmerican,
+        NativeHawaiianOrOtherPacificIslander,
+        White,
+        MultiRace,
+        Other
+    }
+}
