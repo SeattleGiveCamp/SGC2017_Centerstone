f71617cfcba3cf3d0335e09ab9927bf1bcb5a6f2
Frank A. Krueger <fak@praeclarum.org>
2017-10-07 16:48:17 -0700

    Working on Energy page

--- Centerstone/Centerstone/Views/EnergyPage.xaml.cs
+++ Centerstone/Centerstone/Views/EnergyPage.xaml.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Centerstone.Models;
 using Xamarin.Forms;
 
 namespace Centerstone.Views
 {
 	public partial class EnergyPage : ContentPage
 	{
-		public EnergyPage ()
+		public Dictionary<string, HouseholdType> householdTypes = new Dictionary<string, HouseholdType>
+		{
+			{ "1-3 Families", HouseholdType.Family1To3 },
+			{ "4+ Families", HouseholdType.Family4Plus },
+			{ "Hi-rise", HouseholdType.HiRise },
+			{ "Mobilehome", HouseholdType.Mobile },
+			{ "RV", HouseholdType.RV },
+		};
+
+		public EnergyPage (HIF hif)
 		{
 			InitializeComponent ();
+
+			BindingContext = hif;
+
+			InitHouseholdTypeItems ();
+		}
+
+		void InitHouseholdTypeItems ()
+		{
+			//foreach (var name in householdTypes.Values) {
+				HouseholdTypePicker.ItemsSource = new List<HouseholdType> (householdTypes.Values);
+			//}
+		}
+
+		void Handle_Done (object sender, System.EventArgs e)
+		{
+			Navigation.PopToRootAsync (false);
 		}
 	}
 }
