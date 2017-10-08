cc2387422821c5dbaa289f72bd14aff28b611e0b
Donnie <donnie86@gmail.com>
2017-10-08 09:55:27 -0700

    WebScaffold and Mobile SigPad Reference

--- Centerstone/Centerstone.Models/IncomeGuideline.cs
+++ Centerstone/Centerstone.Models/IncomeGuideline.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Centerstone.Models
 {
     public class IncomeGuideline
     {
+        [Key]        
+        public int myKey { get; set; }
+
+        [Display(Name = "Household Size")]
         public int HouseholdSize { get; set; }
-        public double MaxIncome { get; set; }
+
+        [Display(Name ="Maximum Income")]
+        public decimal MaxIncome { get; set; }
     }
 }
--- Centerstone/Centerstone.Web/Controllers/HomeController.cs
+++ Centerstone/Centerstone.Web/Controllers/HomeController.cs
@@ -17,20 +17,6 @@ namespace Centerstone.Web.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         [AllowAnonymous]
         public IActionResult Error()
         {
