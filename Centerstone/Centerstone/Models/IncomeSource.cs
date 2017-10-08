ee2e9e32ee1f8ab6416f282a7e488cdb1efbf2f7
yichiang <yi_chiang@outlook.com>
2017-10-07 18:33:31 -0700

    Update model

--- Centerstone/Centerstone/Models/IncomeSource.cs
+++ Centerstone/Centerstone/Models/IncomeSource.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Centerstone.Helpers;
+using System;
 using System.Collections.ObjectModel;
 
 namespace Centerstone.Models
 {
 	public class IncomeSource
 	{
-		public IncomeSourceType IncomeSourceType { get; set; }
+		public string IncomeSourceType { get; set; }
 
 		public ObservableCollection<Image> Image { get; set; }
-	}
+        public string[] IncomeSourceOptions => IncomeSources.All;
+
+    }
 }
--- Centerstone/Centerstone/Views/PersonPage.xaml.cs
+++ Centerstone/Centerstone/Views/PersonPage.xaml.cs
@@ -11,76 +11,9 @@ namespace Centerstone
 {
     public partial class PersonPage : ContentPage
     {
-        //public event PropertyChangedEventHandler PropertyChanged;
-
-        //private Person person;
-        //public Person Person
-        //{
-        //    get { return this.person;  }
-        //    set
-        //    {
-        //        this.person = value;
-        //        if (PropertyChanged != null)
-        //        {
-        //            PropertyChanged(this,
-        //                new PropertyChangedEventArgs("Person"));
-        //        }
-        //    }
-        //}
+        
         public Person Person { get; set; }
-                                           // public string SelectGender { get; set; }
-                                           //public Dictionary<string, RelationType> relationOptions = new Dictionary<string, RelationType>
-                                           //{
-                                           //    { "Self", RelationType.Self },
-                                           //    { "Spouse", RelationType.Spouse },
-                                           //    { "Partner", RelationType.Partner },
-                                           //    { "Child", RelationType.Child },
-                                           //    { "Other Relative", RelationType.OtherRelative },
-                                           //    { "Other Non Relative", RelationType.OtherNonRelative },
-                                           //};
-                                           //public Dictionary<string, RaceType> racesOptions = new Dictionary<string, RaceType>
-                                           //{
-                                           //    { "American Indian Or Alaskan Native", RaceType.AmericanIndianOrAlaskanNative },
-                                           //    { "Asian", RaceType.Asian },
-                                           //    { "Black Or African American", RaceType.BlackOrAfricanAmerican },
-                                           //    { "Native Hawaiian Or Other Pacific Islander", RaceType.NativeHawaiianOrOtherPacificIslander },
-                                           //    { "White", RaceType.White },
-                                           //    { "Multi Race", RaceType.MultiRace },
-                                           //    { "Other", RaceType.Other },
-                                           //};
-                                           //public string[] genderOptions = Enum.GetValues(typeof(GenderType))
-                                           //                                    .Cast<object>()
-                                           //                                    .Select(x => x.ToString())
-                                           //                                    .ToArray();
-
-        //public Dictionary<string, EthnicityType> ethnicityOptions = new Dictionary<string, EthnicityType>
-        //{
-        //    { "Hispanic Or Latino", EthnicityType.HispanicOrLatino },
-        //    { "Not Hispanic Or Latino", EthnicityType.NotHispanicOrLatino }
-        //};
-        //public Dictionary<string, EducationType> educationTypeOptions = new Dictionary<string, EducationType>
-        //{
-        //    { "0 - 8", EducationType.ZeroToEight },
-        //    { "9-12 Non-Graduate", EducationType.NightToTwelveNonGraduate },
-        //    { "High School Graduate/GED", EducationType.HighSchooOrGED },
-        //    { "12+ Some Post-Secondary", EducationType.MoreThanTwelve },
-        //    { "2 or 4 Year College Graduate", EducationType.TwoToFourYearCollegeGraduate }
-        //};
-
-        //public Dictionary<string, IncomeSourceType> incomeSourceOptions = new Dictionary<string, IncomeSourceType>
-        //{
-        //    { "SSI", IncomeSourceType.SSI },
-        //    { "GA", IncomeSourceType.GA },
-        //    { "VA", IncomeSourceType.VA },
-        //    { "Soc Sec", IncomeSourceType.SocSec },
-        //    { "Military", IncomeSourceType.Military },
-        //    { "EarnedIncome", IncomeSourceType.EarnedIncome },
-        //    { "Pension", IncomeSourceType.Pension },
-        //    { "SelfEmployed", IncomeSourceType.SelfEmployed },
-        //    { "ChildSupport", IncomeSourceType.ChildSupport },
-        //    { "Unemployment", IncomeSourceType.Unemployment },
-        //    { "Other", IncomeSourceType.Other }
-        //};
+        public string[] IncomeSourceOptions { get; set; } = IncomeSources.All;
         public PersonPage(HIF hif, Person person)
         {
             InitializeComponent();
@@ -140,7 +73,7 @@ namespace Centerstone
         public void InitIncomeSourceItems()
         {
             ////Picker picker = IncomeSourceTypePicker;
-            ////picker.ItemsSource = Races.All;
+            //picker.ItemsSource = IncomeSources.All;
 
         }
     }
