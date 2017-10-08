67a63bd128a3d5bf29aa9d5ff8aa54c284e3919a
Frank A. Krueger <fak@praeclarum.org>
2017-10-08 10:20:43 -0700

    Add Application saving and restoring

--- Centerstone/Centerstone/App.xaml.cs
+++ Centerstone/Centerstone/App.xaml.cs
@@ -19,18 +19,32 @@ namespace Centerstone
         {
             InitializeComponent();
 
-			HIF hif = new HIF ();
-
             MobileCenter.Start($"android={Constants.MobileCenterAndroid};" +
                    $"uwp={Constants.MobileCenterUWP};" +
                    $"ios={Constants.MobileCenteriOS}",
                    typeof(Analytics), typeof(Crashes));
 
-
             if (UseMockDataStore)
                 DependencyService.Register<MockDataStore>();
             else
                 DependencyService.Register<HIFCloudDataStore>();
+
+			//
+			// Load Application
+			//
+			var lastPath = Settings.LastApplicationPath;
+			var id = Guid.NewGuid ();
+			var hif = new HIF () {
+				UniqueApplicationId = id,
+			};
+
+			if (!string.IsNullOrEmpty (lastPath)) {
+				var path = System.IO.Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.MyDocuments), lastPath);
+				Console.WriteLine ("TRY READ " + path);
+				if (System.IO.File.Exists (path)) {
+					hif = HIF.ReadFile (path);
+				}
+			}
 
 			MainPage = new NavigationPage (new HomePage (hif));
         }
